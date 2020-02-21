9c3d1c3f94df1c7013d7e1dd895a197863fcd4a1
dmytroholovnia <dmytro.holovnia@gmail.com>
2020-02-21 17:42:08 +0200

    Task8 architecture

---
+++ src/Task8.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Task8 {
+
+//    Найти сумму чисел из рандомно заполненного от 0 до 9 массива [100]
+//    используя в качестве индекса числа Фибоначи. Для остальных чисел найти среднее.
+
+    public static void main(String[] args) {
+
+        //fibonacci array
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        int[] fibonacciArr = new int [20];
+
+        for (int i = 0; i < 2; i++) {
+            fibonacciArr[i] = 1;
+        }
+
+        for(int i = 3; i <= fibonacciArr.length; i++){
+            n2=n0+n1;
+            n0=n1;
+            n1=n2;
+            fibonacciArr[i-1] = n2;
+        }
+
+        int [] randomArrayPP = Helper.getRandomArray(100, 0, 9);
+
+    }
+}
