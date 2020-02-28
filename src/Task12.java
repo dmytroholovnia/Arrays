f53e5b14e8d9cd2abe900c348ff3098ccdd1b243
dmytroholovnia <dmytro.holovnia@gmail.com>
2020-02-28 14:53:12 +0200

    string task 1

---
+++ src/Task12.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Task12 {
+
+    //Найти в рандомно заполненном массиве [4][4][4] чисел 4 элемента наибольших и 4 наименьшим значениями.
+    //Вывести эти элементы на экран, а также вывести сумму всех остальных элементов (4 min и 4 max в сумму не входят).
+
+    public static void main(String[] args) {
+        int[][][] threeDim = new int[4][4][4];
+        int leng = threeDim.length;
+
+        //filling of array
+        for (int i = 0; i < leng; i++) {
+            for (int j = 0; j < leng; j++) {
+                for (int k = 0; k < leng; k++) {
+                    threeDim[i][j][k] = (int) (Math.random() * 9);
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(threeDim));
+
+
+        int minArr[] = new int[3];
+        int maxArr[] = new int[3];
+        boolean isSorted = false;
+
+        do {
+            for (int i = 0; i < leng; i++) {
+                for (int j = 0; j < leng; j++) {
+                    for (int k = 0; k < leng; k++) {
+                        if (k + 1 != leng && threeDim[i][j][k] > threeDim[i][j][k + 1]){
+                            int tempo = threeDim[i][j][k];
+                            threeDim[i][j][k] = threeDim[i][j][k + 1];
+                            threeDim[i][j][k + 1] = tempo;
+                        }
+                    }
+                    for (int k = 0; k < leng; k++) {
+                        if (k + 1 != leng && threeDim[i][j][k] > threeDim[i][j][k + 1]) {
+                            isSorted = false;
+                            break;
+                        } else {
+                            isSorted = true;
+                        }
+                    }
+                }
+
+            }
+        } while (!isSorted);
+        System.out.println(Arrays.deepToString(threeDim));
+
+    }
+}
---
+++ src/lesson2602.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.lang.String;
+
+public class lesson2602 {
+    public static void main(String[] args) {
+        String string = new String("School");
+        String string1 = "Hello, world! My name is Roman =)";
+
+
+        System.out.println(Arrays.toString(string1.split(" ")));
+
+        String[] string2 = string1.split(" ");
+
+        int[] arrLength = new int[string2.length];
+
+
+        for (int i = 0; i < string2.length; i++) {
+            arrLength[i] = string2[i].length();
+        }
+
+        System.out.println(Arrays.toString(arrLength));
+
+    }
+}
